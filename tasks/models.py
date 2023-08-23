7a7545e56433ecfdc4940b34dc93b0ea33d80ffc
Nayara Dornelas <nayaradornelas@id.uff.br>
2023-08-23 00:40:27 -0300

    add a model and some comments

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+##################################################################################################
+""" A model is the single, definitive source of information about your data. 
+It contains the essential fields and behaviors of the data you're storing. 
+Generally, each model maps to a single database table. 
+
+The basics: -Each model is a Python class that subclasses django.db.models.Model;
+.-Each attribute of the model represents a database field."""
+###################################################################################################
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello world')
+    #a render function combines a template with a context dictionary and returns an HttpResponse object with the rendered text.
+    return render(request, 'tasks/list.html')
