d44892ad280dd58686d7f99caed7b3bc7657892c
Nayara Dornelas <nayaradornelas@id.uff.br>
2023-08-22 23:16:29 -0300

    add some comments

--- todo/urls.py
+++ todo/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+############# MY COMMENTS ###########################
+# If you have a view function in your views.py, connect it to a URL inside the urls.py to allow request routing
+""" When a client requests a page by accessing a URL, Django matches the URL pattern by iterating through the list of all the URLs in your project URL configuration.
+If Django matches that particular URL pattern, Django uses the view connected to the URL endpoint.
+The view processes the information and returns an HTTP object to the user. Mainly, the HTTP object includes an HTML page of the URL endpoint requested.
+*Source: https://ngangasn.com/what-are-views-and-urls-in-django-explained-in-simple-terms/#google_vignette"""
+
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tasks.urls'))
 ]
